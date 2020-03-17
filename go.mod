d1950dbad3d9c51bd009a6d60a0f40e46ac34922
zhiganggao <zhiganggao@tencent.com>
2020-03-17 14:51:37 +0800

    finish first go sem code demo

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module go_sem_tool
 
 go 1.12
 
-require github.com/golang/protobuf v1.3.5
+require github.com/golang/protobuf v1.3.5 // indirect
--- sem.go
+++ sem.go
@@ -4,28 +4,49 @@ import (
 	"flag"
 	"log"
 	"syscall"
+	"time"
 	"unsafe"
 )
 
 /*
 #include <sys/sem.h>
 typedef struct sembuf sembuf;
-typedef union semun semun;
+typedef struct _semun {
+        int val;
+} semun;;
 
 */
 import "C"
 
 func semget(key int) int {
-	r1, r2, err := syscall.Syscall(syscall.SYS_SEMGET, uintptr(key),
+	r1, _, _ := syscall.Syscall(syscall.SYS_SEMGET, uintptr(key),
 		uintptr(1), uintptr(00666))
 	if int(r1) < 0 {
-		r1, r2, err = syscall.Syscall(syscall.SYS_SEMGET, uintptr(key),
+		semid, r2, err := syscall.Syscall(syscall.SYS_SEMGET, uintptr(key),
 			uintptr(1), uintptr(C.IPC_CREAT|C.IPC_EXCL|00666))
-		if int(r1) < 0 {
+		if int(semid) < 0 {
 			log.Printf("error:semget error is %v\n", err)
 		}
+		var newInit int
+		newInit = 1
+
+		r1, r2, err := syscall.Syscall6(syscall.SYS_SEMCTL,
+			uintptr(semid),
+			uintptr(0),
+			uintptr(C.SETVAL),
+			uintptr(newInit),
+			uintptr(0), uintptr(0))
+		if int(r1) < 0 {
+			log.Fatal("error:SYS_SEMCTL:", r1, r2, err)
+		}
+		return int(semid)
+
 	} else {
-		log.Printf("success :semget is %v,%v,%v\n", r1, r2, err)
+
+		semid := r1
+
+		return int(semid)
+
 	}
 	return int(r1)
 }
@@ -44,6 +65,17 @@ func semLock(semid int) int {
 	}
 	return int(r1)
 }
+func semShow(semid int) int {
+
+	r1, r2, err := syscall.Syscall(syscall.SYS_SEMCTL,
+		uintptr(semid),
+		uintptr(0),
+		uintptr(C.GETVAL))
+	if int(r1) < 0 {
+		log.Printf("error:semShow error is %v,%v,%v\n", r1, r2, err)
+	}
+	return int(r1)
+}
 
 //int  semctl(int _semid  ,int _semnum,int _cmd  ……);
 var view = flag.Bool("v", false, "get the current value of a sem")
@@ -57,16 +89,22 @@ func main() {
 		flag.Usage()
 		log.Fatal("Must have a key")
 	}
-
 	sem := semget(*key)
 	if sem < 0 {
 		log.Printf("Open Sem Failed with %d!\n", sem)
 		return
 	}
-	ret := semLock(sem)
-	if ret != 0 {
-		log.Fatal("Wait Sem Failed!\n")
+
+	if *view == true {
+		log.Printf("sem show result is %d\n", semShow(sem))
+	} else {
+
+		ret := semLock(sem)
+		if ret != 0 {
+			log.Fatal("Wait Sem Failed!\n")
+		}
+		log.Printf("Wait for Sem %d success and go on!\n", *key)
+		time.Sleep(time.Duration(10) * time.Second)
 	}
-	log.Printf("Wait for Sem %d success and go on!\n", *key)
 
 }
